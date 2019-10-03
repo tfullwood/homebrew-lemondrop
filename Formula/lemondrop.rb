c0c96ed21dfb3acdb4770fc38d9c3365052151e5
Trevor Fullwood <tfullwood@instructure.com>
2019-10-03 15:24:44 -0600

    Init

---
+++ Formula/lemondrop.rb
@@ -0,0 +1,19 @@
+class Lemondrop < Formula
+    desc "Simple CLI to split and prettify large JSON files"
+    homepage "https://github.com/tfullwood/lemondrop"
+    url "https://github.com/tfullwood/lemondrop.git", :revision => "682baa4630cbfbbb102509cd06b28d0c0f15a89e"
+    head "https://github.com/tfullwood/lemondrop.git", :branch => "master"
+  
+    def install
+      bin.install "bin/build/lemondrop"
+    end
+  
+    def caveats; <<~EOS
+      Run `lemondrop [file] <options>` to split and prettify files.
+      EOS
+    end
+  
+    test do
+      system "lemondrop", "--version"
+    end
+  end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# homebrew-lemondrop
-Homebrew tap to install lemondrop
+# Lemondrop Homebrew Tap
+Tap contains a formula to install [Lemondrop](https://github.com/tfullwood/lemondrop)
+
+## Install
+`brew tap tfullwood/lemondrop` then `brew install lemondrop`
