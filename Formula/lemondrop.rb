bc93e6287f71944f7ab3498d946be9b1669d01ef
Trevor Fullwood <tfullwood@instructure.com>
2019-10-04 08:54:46 -0600

    Added version tag

--- Formula/lemondrop.rb
+++ Formula/lemondrop.rb
@@ -1,7 +1,7 @@
 class Lemondrop < Formula
     desc "Simple CLI to split and prettify large JSON files"
     homepage "https://github.com/tfullwood/lemondrop"
-    url "https://github.com/tfullwood/lemondrop.git", :revision => "682baa4630cbfbbb102509cd06b28d0c0f15a89e"
+    url "https://github.com/tfullwood/lemondrop.git", :tag => "v0.0.1", :revision => "682baa4630cbfbbb102509cd06b28d0c0f15a89e"
     head "https://github.com/tfullwood/lemondrop.git", :branch => "master"
   
     def install
